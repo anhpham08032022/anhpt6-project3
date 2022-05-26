3a009386330611b840cc8378f76589822eb6d669
anhpham <pham.anh081188@gmail.com>
2022-05-26 23:11:27 +0700

    add reverseproxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a96c3a139827540e1a95bf7e82671fa3-1909698521.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
